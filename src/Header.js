ac09e8558200d10df193b4c96c1485dc8df0d20c
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-11 15:30:29 -0700

    installed materialize and added icon

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
         <div>
+            <PersonIcon />
             <h2>I am a header</h2>
         </div>
     )
