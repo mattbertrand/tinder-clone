f0622220a5767b1861184350dfd768a2e3062e3b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-11 15:22:09 -0700

    added a Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build a Tinder clone!</h1>
+      <Header />
     </div>
   );
 }
