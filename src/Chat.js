cc76d7101dc25d7b0fcd3af47555586522db9252
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-12 12:32:51 -0700

    styled the chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './Chat.css'
 import Avatar from '@material-ui/core/Avatar'
 
 function Chat({ name, message, profilePic, timestamp}) {
     return (
         <div className="chat">
             <Avatar className="chat__image" alt={name} src={profilePic}/>
-            <div>
+            <div className="chat__details">
                 <h2>{name}</h2>
                 <p>{message}</p>
             </div>
-                <p>{timestamp}</p>
+                <p className="chat__timestamp">{timestamp}</p>
         </div>
     )
 }
