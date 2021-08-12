60bf3f55cf69ac5af2b74f04f10446c626b8fc1c
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-12 11:42:36 -0700

    imported bottom icons and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           </Route>
           <Route path="/">
             <TinderCards />
+            <SwipeButtons />
           </Route>
 
         </Switch>
