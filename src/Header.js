2cf451ebfe9ba5f9c6d3292bb038c39c91e03010
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-11 22:27:47 -0700

    finished styling the icons

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,20 @@ import React from 'react'
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large"/>
+
+            </IconButton>
             <img className="header__logo"
             src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.jpg" alt="tinder logo"/>
-            <ForumIcon />
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large"/>
+            </IconButton>
         </div>
     )
 }
