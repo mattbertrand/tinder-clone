29c6ea5383d010a2eb4bc44110192c16842c8f70
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-11 22:20:25 -0700

    imported icon and styled

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
-        <div>
+        <div className="header">
             <PersonIcon />
-            <h2>I am a header</h2>
+            <img className="header__logo"
+            src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.jpg" alt="tinder logo"/>
+            <ForumIcon />
         </div>
     )
 }
