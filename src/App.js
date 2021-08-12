cc2d5d9abdf1a95578e3db8bc1fa2bced9556321
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-12 13:12:28 -0700

    added a chatScreen component to render the messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import TinderCards from './TinderCards';
 import SwipeButtons from './SwipeButtons';
 import Chats from './Chats'
+import ChatScreen from './ChatScreen'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -11,6 +12,10 @@ function App() {
     <div className="App">
       <Router>
           <Switch>
+            <Route path="/chat/:person">
+              <Header backButton="/chat"/>
+              <ChatScreen />
+            </Route>
             <Route path="/chat">
               <Header backButton="/"/>
               <Chats />
